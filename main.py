a3a9c70ab778fade0d7ef772a8ec54c0277633f7
pirinth0403 <pirinthapan0403@gmail.com>
2025-08-08 15:32:57 +0100

    this is my first msg

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    # Your main program logic here
+    print("Hello, GitHub!")
+    for i in range(5):
+        print(f"Number {i}")
+
+# This ensures main() runs only when this file is executed directly
+if __name__ == "__main__":
+    main()
