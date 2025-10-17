9762d3c515b9ac4f78a04bd3194c121dcdd35a9c
pirinth0403 <pirinthapan0403@gmail.com>
2025-10-17 16:20:50 +0100

    TUPLE CODING FOR THIS

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-name = [5, 10, 15, 25]
+high_score = ("NIMISH" , 120)
+print(high_score)
+
+high_score = ("HOLLY" , 150)
+print(high_score)
+
+name = high_score[0]
 print(name)
 
+print(len(high_score))
+
+print("HOLLY" in high_score)
+
+print(name[0])
+print(name[0:2])
+print("HOL" in name)
+print(len(name))
