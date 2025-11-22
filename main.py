f9ee048431f9f0681a2a39ad722d228bd623ec84
pirinth0403 <pirinthapan0403@gmail.com>
2025-11-22 04:46:43 +0000

    oop concept

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-def swap_case(s):
-    result = ""
-    for char in s:
-        if char.islower():
-            result += char.upper()
-        elif char.isupper():
-            result += char.lower()
-        else:
-            result += char
-    return result
 
-s = input("Enter a string: ")
-result = swap_case(s)
-print(result)
+
+#OOP CONCEPT
+"""class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):#
+        print(self.firstname, self.lastname)
+
+class Student(Person):
+    def __init__(self, fname, lname, score):
+        super().__init__(fname, lname,)
+        self.score = score
+    def printscore(self):
+        print(self.score)
+x=Student("Michael","Smith","34")
+x.printname()
+x.printscore()"""
+class Person:
+    def __init__(self, fname, lname,age):
+        self.firstname = fname
+        self.lastname = lname
+        self.__age = age
+    def get_age(self):
+        return self.__age
+    def set_age(self, age):
+        if age > 18:
+            self.__age = age
+        else:
+           print("age should be 18")
+
+p1=Person("Michael","Smith","2")
+print(p1.firstname)
+print(p1.lastname)
+print(p1.get_age())
+
+p1.set_age(13)
+print(p1.get_age())
+
+
+
 
 
 
